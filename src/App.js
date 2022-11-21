6489ec817d7defc3bb8d338d4c88e2bdf53ee04f
Simran Chaurasia <simransia07@gmail.com>
2022-11-21 12:34:12 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import FormElement from './Component/FormElement';
+import './Styles/styles.scss'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2 className='container_heading'>Changed border color of an outlined text field <br />
+        from <span className='mui-color'></span>#1975d2 to <span className='changed-color'></span>#1d72e7</h2>
+      <FormElement />
     </div>
   );
 }
---
+++ src/Component/FormElement.jsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+
+
+function FormElement() {
+
+  return (
+    <>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" className='text-field' />
+    </>
+  );
+}
+
+export default FormElement;
